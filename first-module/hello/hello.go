c2552c4a0834d78cec9239b2a53e24f79849169f
ChrisL108 <chrislacaille5@gmail.com>
2022-06-01 03:05:02 -0500

    testing

---
+++ first-module/hello/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	myNum := test(4)
+	fmt.Println(myNum)
+	fmt.Println(message)
+}
+
+func test(num int) int {
+	return num
+}
